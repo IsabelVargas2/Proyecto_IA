46b96bac1be07eb106cd9b366347cdb5be206f6a
IsabelVargas2 <165979808+IsabelVargas2@users.noreply.github.com>
2025-03-26 17:51:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 import streamlit as st
 import joblib
 import numpy as np
